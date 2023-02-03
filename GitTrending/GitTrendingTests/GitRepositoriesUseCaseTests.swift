e587488e0fae809d765a1e2a23be674c04c3de97
Muhammad Qasim Majeed <qasim_majeed2003@yahoo.com>
2023-02-03 22:02:09 +0400

    GitRepositoriesUseCaseTests is created

---
+++ GitTrending/GitTrendingTests/GitRepositoriesUseCaseTests.swift
@@ -0,0 +1,19 @@
+//
+//  GitRepositoriesUseCaseTests.swift
+//  GitTrendingTests
+//
+//  Created by Muhammad Qasim Majeed on 03/02/2023.
+//
+
+import XCTest
+
+final class GitRepositoriesUseCaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override  func tearDown() {
+        super.tearDown()
+    }
+}
