21eab0c41b57432cf1dcfebccf7447f7999d2753
Muhammad Qasim Majeed <qasim_majeed2003@yahoo.com>
2023-02-04 15:46:58 +0400

    Code refactored - The ViewController is removed The extension for UIStoryboard is added into to define storyboard easily The test case for ViewController is also refactored

---
+++ GitTrending/GitTrending/Common/UIStoryboard+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  UIStoryboard+Extensions.swift
+//  GitTrending
+//
+//  Created by Muhammad Qasim Majeed on 04/02/2023.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    /// Define all the storyboards name here
+    enum Name: String {
+        case gitRepositories = "GitRepositories"
+    }
+    
+    convenience init(name: Name, bundle: Bundle? = nil) {
+      self.init(name: name.rawValue, bundle: bundle)
+    }
+}
--- GitTrending/GitTrendingTests/Tests/GitRepositoriesViewControllerTests.swift
+++ GitTrending/GitTrendingTests/Tests/GitRepositoriesViewControllerTests.swift
@@ -9,24 +9,24 @@ import XCTest
 @testable import GitTrending
 
 final class GitRepositoriesViewControllerTests: XCTestCase {
+    // MARK: - Private Properties
+    private var sut: GitRepositoriesViewController!
+    private var storyBoard: UIStoryboard!
     
     // MARK: - XCTestCase
     override func setUp() {
         super.setUp()
+        storyBoard = UIStoryboard(name: .gitRepositories)
+        sut = storyBoard.instantiateViewController(withIdentifier: "GitRepositoriesViewController") as? GitRepositoriesViewController
     }
     
     override func tearDown() {
         super.tearDown()
+        sut = nil
+        storyBoard = nil
     }
     
     func testGitRepositoriesViewController_WhenCreated_ShouldReturnController() {
-        //Arrange
-        let sut: GitRepositoriesViewController!
-        let storyBoard = UIStoryboard(name: "GitRepositories", bundle: nil)
-        
-        //Act
-        sut = storyBoard.instantiateViewController(withIdentifier: "GitRepositoriesViewController") as? GitRepositoriesViewController
-        
         //Assert
         XCTAssertNotNil(sut, "The controller should be exits in the storyboard")
         XCTAssertNotNil(storyBoard, "The name of the story board should be proper and exits")
