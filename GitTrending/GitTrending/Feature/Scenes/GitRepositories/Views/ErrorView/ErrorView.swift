da9ccc05ee9b0137095d496096508719e902164e
Muhammad Qasim Majeed <qasim_majeed2003@yahoo.com>
2023-02-05 16:00:31 +0400

    To verify the error views the test case is added

--- GitTrending/GitTrending/Feature/Scenes/GitRepositories/Views/ErrorView/ErrorView.swift
+++ GitTrending/GitTrending/Feature/Scenes/GitRepositories/Views/ErrorView/ErrorView.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ErrorView: UIView {
+    // MARK: - Properties
+    @IBOutlet weak var animationView: UIView!
+    @IBOutlet weak var someThingWrongLabel: UILabel!
+    @IBOutlet weak var errorDetailLabel: UILabel!
+    @IBOutlet weak var retryButton: UIButton!
+    
+    // MARK: - Methods
+    
     class func loadViewFromXib() -> ErrorView? {
         return UINib(nibName: "ErrorView", bundle: Bundle(for: ErrorView.self)).instantiate(withOwner: self, options: nil)[0] as? ErrorView
     }
--- GitTrending/GitTrendingTests/Tests/ErrorViewTests.swift
+++ GitTrending/GitTrendingTests/Tests/ErrorViewTests.swift
@@ -24,4 +24,21 @@ final class ErrorViewTests: XCTestCase {
         //Assert
         XCTAssertNotNil(sut)
     }
+    
+    func testErrorView_WhenLoads_ShouldHaveDefaultValues() throws {
+        //Arrange
+        let sut: ErrorView = ErrorView.loadViewFromXib()!
+        _ = try XCTUnwrap(sut.animationView, "The animationView IBOutlet should be connected")
+        let someThingWrongLabel = try XCTUnwrap(sut.someThingWrongLabel, "The someThingWrongLabel IBOutlet should be connected")
+        let errorDetailLabel = try XCTUnwrap(sut.errorDetailLabel, "The errorDetailLabel IBOutlet should be connected")
+        let retryButton = try XCTUnwrap(sut.retryButton, "The retryButton IBOutlet should")
+        
+        //Assert
+        XCTAssertEqual(someThingWrongLabel.text, "Something went wrong..", "The someThingWrongLabel not empty when the cell load")
+        XCTAssertEqual(errorDetailLabel.text, "An alien is probably blocking your signal.", "The errorDetailLabel not empty when the cell load")
+        XCTAssertEqual(retryButton.titleLabel?.text, "RETRY", "The retryButton not empty when the cell load")
+       
+        
+        
+    }
 }
