ebaa415f3a8eec13364277f57b0f0ca83208e608
Muhammad Qasim Majeed <qasim_majeed2003@yahoo.com>
2023-02-05 16:07:51 +0400

    code refactored

--- GitTrending/GitTrendingTests/Tests/ErrorViewTests.swift
+++ GitTrending/GitTrendingTests/Tests/ErrorViewTests.swift
@@ -9,25 +9,24 @@ import XCTest
 @testable import GitTrending
 
 final class ErrorViewTests: XCTestCase {
-    
+    // MARK: - Private Properties
+    private var sut:ErrorView!
     override func setUp() {
         super.setUp()
+        sut = ErrorView.loadViewFromXib()
     }
     
     override func tearDown() {
         super.tearDown()
+        sut = nil
     }
     
     func testErrorView_WhenLoadXib_ShouldReturnView() {
-        //Arrange
-        let sut = ErrorView.loadViewFromXib()
-        //Assert
         XCTAssertNotNil(sut)
     }
     
     func testErrorView_WhenLoads_ShouldHaveDefaultValues() throws {
-        //Arrange
-        let sut: ErrorView = ErrorView.loadViewFromXib()!
+        //ArrangeU
         _ = try XCTUnwrap(sut.animationView, "The animationView IBOutlet should be connected")
         let someThingWrongLabel = try XCTUnwrap(sut.someThingWrongLabel, "The someThingWrongLabel IBOutlet should be connected")
         let errorDetailLabel = try XCTUnwrap(sut.errorDetailLabel, "The errorDetailLabel IBOutlet should be connected")
@@ -37,8 +36,5 @@ final class ErrorViewTests: XCTestCase {
         XCTAssertEqual(someThingWrongLabel.text, "Something went wrong..", "The someThingWrongLabel not empty when the cell load")
         XCTAssertEqual(errorDetailLabel.text, "An alien is probably blocking your signal.", "The errorDetailLabel not empty when the cell load")
         XCTAssertEqual(retryButton.titleLabel?.text, "RETRY", "The retryButton not empty when the cell load")
-       
-        
-        
     }
 }
